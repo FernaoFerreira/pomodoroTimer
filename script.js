faaa7829da2c76dbfd69af058d5108e1a5cf8b80
FernaoFerreira <fernaoqf@gmail.com>
2025-06-12 11:38:47 -0300

    Add: Reset button resets display

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
     let remainingTime;
     let intervalId;
+    let isPaused = false;
 
     const timer = document.getElementById('timecontainer');
+    const pause = document.getElementById('pause');
+
     function startTimer(){
         clearInterval(intervalId);
         intervalId = setInterval(() => {
@@ -16,11 +19,40 @@
         }, 1000);
     }
 
+    function stopTimer(){
+        clearInterval(intervalId);
+    }
+    
     function updateTimer(minutes, seconds){
         seconds = seconds.toString().padStart(2,'0');
         timer.innerHTML = `${minutes}m ${seconds}s`;
     }
+
+
     document.getElementById("start").onclick = function(){
         remainingTime = Number(document.getElementById("timebox").value)  * 60;
+        initialTime = remainingTime;
         startTimer();
+    }
+
+    document.getElementById("pause").onclick = function(){
+        if(!isPaused){
+            stopTimer();
+            pause.innerHTML = "Resume";
+            isPaused = true;
+        }
+        else{
+            startTimer();
+            pause.innerHTML = "Pause";
+            isPaused = false;
+        }
+    }
+
+    document.getElementById("reset").onclick = function(){
+        clearInterval(intervalId);
+        isPaused = false;
+
+        let minutes = Math.floor(initialTime/60); 
+        let seconds = initialTime % 60;
+        updateTimer(minutes, seconds);
     }
